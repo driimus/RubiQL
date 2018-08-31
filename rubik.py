29d26c5370efeecb09bc994667aad7602b423dad
driimus <driimus@vivaldi.net>
2018-08-31 20:47:11 +0000

    added default sets of moves

--- rubik.py
+++ rubik.py
@@ -1,5 +1,5 @@
 from color import cprint as colorPrint
-
+import time
 class Cube:
 	
 	colors = ('w','r','g','o','b','y',)
@@ -8,11 +8,21 @@ class Cube:
 		self.sideLength = sideLength
 		self.sides = sides
 		self.faces = []
-		self.generate()
+		self.indexes = [i for i in range(sides)]
+		self.evens = []
+		self.odds = []
+		self.__generate__()
 		print("Initialised a solved %s cube." %(3*("x%s" %(sideLength)))[1:])
 		self.niceDisplay()
-	
-	def generate(self):
+		
+	def __indexate__(self):
+		for i in range(sides):
+			if i==0 or i%2==1:
+				odds.append(i)
+			else:
+				evens.append(i)
+				
+	def __generate__(self):
 		for face in range(self.sides):
 # 			color for that respective solved face
 			color = Cube.colors[face]
@@ -44,11 +54,206 @@ class Cube:
 				print("  ",end="")
 			print("\n")
 	
-	
 	def niceDisplay(self):		
 		self.display(self.faces[0:1])
 		self.display(self.faces[1:-1])
 		self.display(self.faces[-1:])
-		print("\n")
 		
-rubik = Cube(3)
+	def __spinYline__(self,faceIndex,topIndex,line):
+		start = self.faces[faceIndex][line].copy()
+		self.faces[faceIndex][line] = self.faces[faceIndex+1][line]
+		self.faces[faceIndex+1][line] = self.faces[faceIndex+2][line]
+		self.faces[faceIndex+2][line] = self.faces[faceIndex+3][line]
+		self.faces[faceIndex+3][line] = start
+		print("Moved middle section 90 degrees.")
+		
+	def __reverseSpinYline__(self,faceIndex,topIndex,line):
+		start = self.faces[faceIndex+3][line].copy()
+		self.faces[faceIndex+3][line] = self.faces[faceIndex+2][line]
+		self.faces[faceIndex+2][line] = self.faces[faceIndex+1][line]
+		self.faces[faceIndex+1][line] = self.faces[faceIndex][line]
+		self.faces[faceIndex][line] = start
+		
+# '''
+# -------------------- MADE FOR ROTATING A SINGLE FACE -------------------
+# '''
+	
+	def __rotateFace__(self,faceIndex):
+		newFace = []
+		for vLine in range(self.sideLength):
+			newLine = []
+			for line in self.faces[faceIndex][::-1]:
+				newLine.append(line[vLine])
+			newFace.append(newLine)
+		self.faces[faceIndex] = newFace
+		
+	def __counterRotateFace__(self,faceIndex):
+		newFace = []
+		for vLine in range(self.sideLength-1,-1,-1):
+			newLine = []
+			for line in self.faces[faceIndex]:
+				newLine.append(line[vLine])
+			newFace.append(newLine)
+		self.faces[faceIndex] = newFace
+	
+# '''
+# ------------------------------------------------------------------------
+# '''
+# '''
+# ---------------- ROTATING THE CUBE ON IT's AXIS ------------------------
+# '''
+# 	CLOCKWISE ROTATIONS
+	def __spinEnds__(self,topIndex,bottomIndex):
+		self.__rotateFace__(topIndex)
+		self.__counterRotateFace__(bottomIndex)
+	
+	def faceSwap(self,iList=[],reverse=False):
+		if reverse:
+			iList.reverse()
+		start = self.faces[iList[0]].copy()
+		for index in range(len(iList)-1):
+			self.faces[iList[index]] = self.faces[iList[index+1]]
+		self.faces[iList[-1]] = start
+		
+			
+	def rotateXaxis(self,topIndex=2,bottomIndex=4,back=3,r=False):
+		self.__spinEnds__(topIndex,bottomIndex)
+		
+		f= [0,1,5,3]
+		self.__rotateFace__(back)
+		self.__rotateFace__(back)
+		
+		self.faceSwap(f,r)
+		
+# 		self.niceDisplay()
+	
+	def rotateYaxis(self,topIndex=0,bottomIndex=5,lastIndex=4,firstIndex=1):
+		self.__spinEnds__(topIndex,bottomIndex)
+		self.faces.insert(lastIndex,self.faces.pop(firstIndex))
+# 		self.niceDisplay()
+		
+	def rotateZaxis(self,topIndex=3,bottomIndex=1,reverse=False):
+		self.__spinEnds__(topIndex,bottomIndex)
+		self.counterRotateYaxis()
+		time.sleep(1)
+		if not reverse:
+			self.rotateXaxis()
+		else:
+			self.counterRotateXaxis()
+		time.sleep(1)
+		
+		self.rotateYaxis()
+		
+	
+# 	COUNTERCLOCKWISE ROTATIONS
+	
+	def counterRotateXaxis(self,topIndex=4,bottomIndex=2,back=5):
+		self.rotateXaxis(topIndex,bottomIndex,back,True)
+	
+	def counterRotateYaxis(self,topIndex=4,bottomIndex=2):
+		self.rotateYaxis(5,0,1,4)
+		
+	def counterRotateZaxis(self):
+		self.rotateZaxis(3,1,True)
+# '''
+# ------------------------------------------------------------------------
+# '''
+# 	############ MOVES THAT CAN BE PERFORMED ##################
+# ''' moves on the Y axis '''
+	def moveU(self,faceIndex=1,topIndex=0,line=0):
+		self.__spinYline__(faceIndex,topIndex,line)
+		self.__rotateFace__(topIndex)
+# 		self.niceDisplay()
+		
+	def moveM(self,faceIndex=1,topIndex=0,line=1):
+		self.__spinYline__(faceIndex,topIndex,line)
+# 		self.niceDisplay()
+		
+	def moveD(self,faceIndex=1,bottomIndex=5,line=2):
+		self.__spinYline__(faceIndex,bottomIndex,line)
+		self.__counterRotateFace__(bottomIndex)
+# 		self.niceDisplay()
+		
+	def moveR(self,reverse=False,faceIndex=1,bottomIndex=5,line=2):
+		self.counterRotateZaxis()
+		if reverse:
+			self.revU()
+		else:
+			self.moveU()
+		self.rotateZaxis()
+# 		self.niceDisplay()
+		
+	def moveL(self,reverse=False):
+		self.rotateZaxis()
+		
+		if not reverse:
+			self.moveU()
+		else:
+			self.revU()
+			
+		self.counterRotateZaxis()
+
+		
+	def revU(self,faceIndex=1,topIndex=0,line=0):
+		self.__reverseSpinYline__(faceIndex,topIndex,line)
+		self.__counterRotateFace__(topIndex)
+# 		self.niceDisplay()
+		
+	def revM(self,faceIndex=1,topIndex=0,line=1):
+		self.__reverseSpinYline__(faceIndex,topIndex,line)
+# 		self.niceDisplay()
+
+	def revD(self,faceIndex=1,bottomIndex=5,line=2):
+		self.__reverseSpinYline__(faceIndex,bottomIndex,line)
+		self.__rotateFace__(bottomIndex)
+# 		self.niceDisplay()
+
+	def revR(self):
+		self.moveR(True)
+	
+	def revL(self):
+		self.moveL(True)
+
+
+if __name__ == "__main__":
+	rubik = Cube(3)
+
+	msg = input("move:")
+	while msg:
+		if msg=="X":
+			rubik.rotateXaxis()
+		if msg=="X'":
+			rubik.counterRotateXaxis()
+		if msg=="Y":
+			rubik.rotateYaxis()
+		if msg=="Y'":
+			rubik.counterRotateYaxis()
+		if msg=="Z":
+			rubik.rotateZaxis()
+		if msg=="Z'":
+			rubik.counterRotateZaxis()
+			
+		if msg=="U":
+			rubik.moveU()
+		if msg=="U'":
+			rubik.revU()
+		if msg=="M":
+			rubik.moveM()
+		if msg=="M'":
+			rubik.revM()
+		if msg=="D":
+			rubik.moveD()
+		if msg=="D'":
+			rubik.revD()
+		if msg=="R":
+			rubik.moveR()
+		if msg=="R'":
+			rubik.revR()
+		if msg=="L":
+			rubik.moveL()
+		if msg=="L'":
+			rubik.revL()
+		rubik.niceDisplay()
+		msg = input("move:")
+		
+		
